7abcb8ed57c595465b3aea6c94c51b1856e97d62
Au <auau0197@gmail.com>
2022-12-09 16:07:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
+    int number = 1, E = 0, O = 0;
+    while(number != 0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> number;
+    if(number%2 != 0){
+    O = O+1;
+    }else{
+    E = E+1;
+    }
+}
+    cout << "#Even numbers = " << E << "\n";
+    cout << "#Odd numbers = " << O;
     return 0;
 }
